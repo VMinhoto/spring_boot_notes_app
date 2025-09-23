1071830e43e610287335d6abb2accf88deb0619b
Vitor Minhoto <vitor.minhoto@inesctec.pt>
2025-09-23 16:19:56 +0100

    Store Refresh Token functionality.

--- src/main/kotlin/com/example/spring_boot_notes_app/security/AuthService.kt
+++ src/main/kotlin/com/example/spring_boot_notes_app/security/AuthService.kt
@@ -1,15 +1,24 @@
 package com.example.spring_boot_notes_app.security
 
+import com.example.spring_boot_notes_app.database.model.RefreshToken
 import com.example.spring_boot_notes_app.database.model.User
+import com.example.spring_boot_notes_app.database.repository.RefreshTokenRepository
 import com.example.spring_boot_notes_app.database.repository.UserRepository
+import org.apache.logging.log4j.message.Message
+import org.bson.types.ObjectId
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.stereotype.Service
+import java.security.MessageDigest
+import java.time.Instant
+import java.util.Base64
+
 
 @Service
 class AuthService(
     private val jwtService: JwtService,
     private val userRepository: UserRepository,
-    private val hashEncoder: HashEncoder
+    private val hashEncoder: HashEncoder,
+    private val refreshTokenRepository: RefreshTokenRepository
 ) {
     data class TokenPair(
         val accessToken: String,
@@ -35,10 +44,32 @@ class AuthService(
         val newAccessToken = jwtService.generateAccessToken(user.id.toHexString())
         val newRefreshToken = jwtService.generateRefreshToken(user.id.toHexString())
 
+        storeRefreshToken(user.id, newRefreshToken)
+
         return TokenPair(
             accessToken = newAccessToken,
             refreshToken = newRefreshToken
         )
+    }
+
+    private fun storeRefreshToken(userId: ObjectId, rawRefreshToken: String) {
+        val hashed = hashToken(rawRefreshToken)
+        val expiryMs = jwtService.refreshTokenValidityMs
+        val expireAt = Instant.now().plusMillis(expiryMs)
+
+        refreshTokenRepository.save(
+            RefreshToken(
+                userId = userId,
+                expiresAt = expireAt,
+                hashed
+            )
+        )
+    }
+
+    private fun hashToken(token: String): String {
+        val digest = MessageDigest.getInstance("SHA-256")
+        val hashBytes = digest.digest(token.toByteArray())
+        return Base64.getEncoder().encodeToString(hashBytes)
     }
 
 }
