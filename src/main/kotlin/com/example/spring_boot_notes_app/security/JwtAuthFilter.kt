36da784fddd7b29916213295a3d639a24dccb774
Vitor Minhoto <vitor.minhoto@inesctec.pt>
2025-09-23 15:39:21 +0100

    JwtFilter to check the tokens

--- src/main/kotlin/com/example/spring_boot_notes_app/security/JwtAuthFilter.kt
+++ src/main/kotlin/com/example/spring_boot_notes_app/security/JwtAuthFilter.kt
@@ -1,4 +1,30 @@
 package com.example.spring_boot_notes_app.security
 
-class JwtAuthFilter {
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+
+class JwtAuthFilter(
+    private val jwtService: JwtService
+): OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val authHeader = request.getHeader("Authorization")
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            if(jwtService.validateAccessToken(authHeader)) {
+                val userId = jwtService.getUserIdFromToken(authHeader)
+                val auth = UsernamePasswordAuthenticationToken(userId, null)
+                SecurityContextHolder.getContext().authentication = auth
+            }
+        }
+
+        filterChain.doFilter(request, response)
+    }
 }
