391712a0b82b271366b5899cbb8e418e823eab4f
Vitor Minhoto <vitor.minhoto@inesctec.pt>
2025-09-23 16:00:22 +0100

    Created a data class hosting userId, expiresAt, hashedRefreshToken, and createdAt

--- src/main/kotlin/com/example/spring_boot_notes_app/database/model/RefreshTokens.kt
+++ src/main/kotlin/com/example/spring_boot_notes_app/database/model/RefreshTokens.kt
@@ -1,3 +1,15 @@
 package com.example.spring_boot_notes_app.database.model
 
-data class RefreshTokens()
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
+
+@Document("refreshTokens")
+data class RefreshTokens(
+    val userId: ObjectId,
+    @Indexed(expireAfter = "0s")
+    val expiresAt: Instant,
+    val hashedToken: String,
+    val createdAt: Instant = Instant.now(),
+    )
