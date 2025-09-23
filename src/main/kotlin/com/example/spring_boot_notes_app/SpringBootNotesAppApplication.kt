9b1a4ef085253b105368d4e108f76c1595d602ce
Vitor Minhoto <vitor.minhoto@inesctec.pt>
2025-09-23 11:56:52 +0100

    1st commit

---
+++ src/main/kotlin/com/example/spring_boot_notes_app/SpringBootNotesAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_boot_notes_app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootNotesAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootNotesAppApplication>(*args)
+}
